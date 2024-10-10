5dad0930b6af80e8d5fb0755c86a1ac058073714
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-10-10 10:19:49 +0200

    create model and entity order

---
+++ model/repository/OrderRepository.php
@@ -0,0 +1,20 @@
+<?php 
+
+require_once './model/entity/Order.php';
+
+class OrderRepository {
+
+    public function persist(Order $order): Order {
+        $_SESSION['order'] = $order;
+        return $order
+    }
+
+    public function find(): ?Order {
+        if (!isset($_SESSION['order'])) {
+            return null;
+        }
+
+        return $_SESSION['order'];
+    }
+    
+}
